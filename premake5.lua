69bb705ef0e1c5a714f89ecf8f14e5b64c943176
CleverSource <stunthound@gmail.com>
2020-07-26 00:16:02 -0400

    Added premake and generated projects.

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Honey"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Honey"
+	location "Honey"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.17134.0"
+
+		defines
+		{
+			"HN_PLATFORM_WINDOWS",
+			"HN_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "HN_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "HN_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "HN_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Honey/vendor/spdlog/include",
+		"Honey/src"
+	}
+
+	links
+	{
+		"Honey"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.17134.0"
+
+		defines
+		{
+			"HN_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "HN_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "HN_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "HN_DIST"
+		optimize "On"
