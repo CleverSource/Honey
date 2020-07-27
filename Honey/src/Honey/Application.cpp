d0dbf3500b7a1d3f996e912d69a93fe50b7d10e8
CleverSource <stunthound@gmail.com>
2020-07-27 00:45:26 -0400

    Added event system + basic test.

--- Honey/src/Honey/Application.cpp
+++ Honey/src/Honey/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "Honey/Events/ApplicationEvent.h"
+#include "Honey/Log.h"
+
 namespace Honey {
 
 	Application::Application()
@@ -13,6 +16,16 @@ namespace Honey {
 
 	void Application::Run()
 	{
+		WindowResizeEvent e(1280, 720);
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			HN_TRACE(e);
+		}
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			HN_TRACE(e);
+		}
+
 		while (true);
 	}
 
