761f161144cb2109b9bf0a658bf89ca0a056ec0e
CleverSource <stunthound@gmail.com>
2020-08-03 13:03:02 -0400

    Added events to ImGui layer.

--- Honey/src/Honey/Events/Event.h
+++ Honey/src/Honey/Events/Event.h
@@ -15,7 +15,7 @@ namespace Honey {
 		None = 0,
 		WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
 		AppTick, AppUpdate, AppRender,
-		KeyPressed, KeyReleased,
+		KeyPressed, KeyReleased, KeyTyped,
 		MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
 	};
 
--- Honey/src/Honey/ImGui/ImGuiLayer.h
+++ Honey/src/Honey/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Honey/Layer.h"
 
+#include "Honey/Events/ApplicationEvent.h"
+#include "Honey/Events/KeyEvent.h"
+#include "Honey/Events/MouseEvent.h"
+
 namespace Honey {
 
 	class HONEY_API ImGuiLayer : public Layer
@@ -14,6 +18,15 @@ namespace Honey {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	private:
 		float m_Time = 0.0f;
 	};
