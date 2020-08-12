f303fa016e39f9dff5efbf44e936bffb92e4d4cb
CleverSource <stunthound@gmail.com>
2020-08-11 23:47:31 -0400

    Layer's OnAttach and OnDetach methods are now called inside LayerStack

--- Honey/src/Honey/Application.cpp
+++ Honey/src/Honey/Application.cpp
@@ -32,13 +32,11 @@ namespace Honey {
 	void Application::PushLayer(Layer* layer)
 	{
 		m_LayerStack.PushLayer(layer);
-		layer->OnAttach();
 	}
 
 	void Application::PushOverlay(Layer* layer)
 	{
 		m_LayerStack.PushOverlay(layer);
-		layer->OnAttach();
 	}
 
 	void Application::OnEvent(Event& e)
--- Honey/src/Honey/LayerStack.cpp
+++ Honey/src/Honey/LayerStack.cpp
@@ -17,11 +17,13 @@ namespace Honey {
 	{
 		m_Layers.emplace(m_Layers.begin() + m_LayerInsertIndex, layer);
 		m_LayerInsertIndex++;
+		layer->OnAttach();
 	}
 
 	void LayerStack::PushOverlay(Layer* overlay)
 	{
 		m_Layers.emplace_back(overlay);
+		overlay->OnAttach();
 	}
 
 	void LayerStack::PopLayer(Layer* layer)
@@ -31,6 +33,7 @@ namespace Honey {
 		{
 			m_Layers.erase(it);
 			m_LayerInsertIndex--;
+			layer->OnDetach();
 		}
 	}
 
@@ -38,7 +41,10 @@ namespace Honey {
 	{
 		auto it = std::find(m_Layers.begin(), m_Layers.end(), overlay);
 		if (it != m_Layers.end())
+		{
 			m_Layers.erase(it);
+			overlay->OnDetach();
+		}
 	}
 
 }
