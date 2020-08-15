8dced0c4833561faba5787361e7d5a33e1f57db4
CleverSource <stunthound@gmail.com>
2020-08-15 15:53:39 -0400

    Changed macro name

--- Honey/src/Honey/Log.h
+++ Honey/src/Honey/Log.h
@@ -25,11 +25,11 @@ namespace Honey {
 #define HN_CORE_INFO(...)     ::Honey::Log::GetCoreLogger()->info(__VA_ARGS__)
 #define HN_CORE_WARN(...)     ::Honey::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define HN_CORE_ERROR(...)    ::Honey::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define HN_CORE_FATAL(...)    ::Honey::Log::GetCoreLogger()->critical(__VA_ARGS__)
+#define HN_CORE_CRITICAL(...) ::Honey::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 // Client log macros
 #define HN_TRACE(...)         ::Honey::Log::GetClientLogger()->trace(__VA_ARGS__)
 #define HN_INFO(...)          ::Honey::Log::GetClientLogger()->info(__VA_ARGS__)
 #define HN_WARN(...)          ::Honey::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define HN_ERROR(...)         ::Honey::Log::GetClientLogger()->error(__VA_ARGS__)
-#define HN_FATAL(...)         ::Honey::Log::GetClientLogger()->critical(__VA_ARGS__)
+#define HN_CRITICAL(...)      ::Honey::Log::GetClientLogger()->critical(__VA_ARGS__)
