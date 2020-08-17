c5864efaf16a6afd8ac70fe82bf0d35caea11956
CleverSource <stunthound@gmail.com>
2020-08-17 05:07:31 -0400

    Added Honey::Scope and Honey::Ref definitions

--- Honey/src/Honey/Core.h
+++ Honey/src/Honey/Core.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <memory>
+
 #ifdef HN_PLATFORM_WINDOWS
 #if HN_DYNAMIC_LINK
 	#ifdef HN_BUILD_DLL
@@ -29,3 +31,13 @@
 #define BIT(x) (1 << x)
 
 #define HN_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+
+namespace Honey {
+
+	template<typename T>
+	using Scope = std::unique_ptr<T>;
+
+	template<typename T>
+	using Ref = std::shared_ptr<T>;
+
+}
