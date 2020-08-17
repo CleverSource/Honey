ac14235dde6c64379f865d6ee44db1d8a7e8dcf0
CleverSource <stunthound@gmail.com>
2020-08-17 04:08:57 -0400

    Moved OpenGL shader implementation to OpenGLShader class + added more uniform functions

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,14 +1,17 @@
 #include <Honey.h>
 
+#include "Platform/OpenGL/OpenGLShader.h"
+
 #include "imgui/imgui.h"
 
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class ExampleLayer : public Honey::Layer
 {
 public:
 	ExampleLayer()
-		: Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f), m_SquarePosition(0.0f)
+		: Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f)
 	{
 		m_VertexArray.reset(Honey::VertexArray::Create());
 
@@ -88,9 +91,9 @@ public:
 			}
 		)";
 
-		m_Shader.reset(new Honey::Shader(vertexSrc, fragmentSrc));
+		m_Shader.reset(Honey::Shader::Create(vertexSrc, fragmentSrc));
 
-		std::string blueShaderVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 
 			layout(location = 0) in vec3 a_Position;
@@ -107,20 +110,22 @@ public:
 			}
 		)";
 
-		std::string blueShaderFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
 
+			uniform vec3 u_Color;
+
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = vec4(u_Color, 1.0);
 			}
 		)";
 
-		m_BlueShader.reset(new Honey::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
+		m_FlatColorShader.reset(Honey::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 
 	void OnUpdate(Honey::Timestep ts) override
@@ -150,13 +155,16 @@ public:
 
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
+		std::dynamic_pointer_cast<Honey::OpenGLShader>(m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<Honey::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
 		for (int y = 0; y < 20; y++)
 		{
 			for (int x = 0; x < 20; x++)
 			{
 				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
 				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-				Honey::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+				Honey::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
 			}
 		}
 
@@ -167,7 +175,9 @@ public:
 
 	virtual void OnImGuiRender() override
 	{
-
+		ImGui::Begin("Settings");
+		ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+		ImGui::End();
 	}
 
 	void OnEvent(Honey::Event& event) override
@@ -177,7 +187,7 @@ private:
 	std::shared_ptr<Honey::Shader> m_Shader;
 	std::shared_ptr<Honey::VertexArray> m_VertexArray;
 
-	std::shared_ptr<Honey::Shader> m_BlueShader;
+	std::shared_ptr<Honey::Shader> m_FlatColorShader;
 	std::shared_ptr<Honey::VertexArray> m_SquareVA;
 
 	Honey::OrthographicCamera m_Camera;
@@ -186,6 +196,8 @@ private:
 
 	float m_CameraRotation = 0.0f;
 	float m_CameraRotationSpeed = 180.0f;
+
+	glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 };
 
 class Sandbox : public Honey::Application
