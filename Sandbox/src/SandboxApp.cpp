af46dc68608f905029f7e8f1956a139edc087521
CleverSource <stunthound@gmail.com>
2020-08-18 11:34:10 -0400

    Added support for blending, and loading both RGB and RGBA textures

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -164,6 +164,7 @@ public:
 		m_TextureShader.reset(Honey::Shader::Create(textureShaderVertexSrc, textureShaderFragmentSrc));
 
 		m_Texture = Honey::Texture2D::Create("assets/textures/Checkerboard.png");
+		m_CleverLogoTexture = Honey::Texture2D::Create("assets/textures/CleverLogo.png");
 
 		std::dynamic_pointer_cast<Honey::OpenGLShader>(m_TextureShader)->Bind();
 		std::dynamic_pointer_cast<Honey::OpenGLShader>(m_TextureShader)->UploadUniformInt("u_Texture", 0);
@@ -211,6 +212,8 @@ public:
 
 		m_Texture->Bind();
 		Honey::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
+		m_CleverLogoTexture->Bind();
+		Honey::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
 
 		// Triangle
 		// Honey::Renderer::Submit(m_Shader, m_VertexArray);
@@ -235,7 +238,7 @@ private:
 	Honey::Ref<Honey::Shader> m_FlatColorShader, m_TextureShader;
 	Honey::Ref<Honey::VertexArray> m_SquareVA;
 
-	Honey::Ref<Honey::Texture2D> m_Texture;
+	Honey::Ref<Honey::Texture2D> m_Texture, m_CleverLogoTexture;
 
 	Honey::OrthographicCamera m_Camera;
 	glm::vec3 m_CameraPosition;
