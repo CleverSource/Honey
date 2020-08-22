460d138d6b4dd2d0cd8288200b8aabaa0b7783fb
CleverSource <stunthound@gmail.com>
2020-08-22 13:10:58 -0400

    Automated platform detection using predefined macro's

--- Honey/src/Honey/Core/Core.h
+++ Honey/src/Honey/Core/Core.h
@@ -2,19 +2,61 @@
 
 #include <memory>
 
-#ifdef HN_PLATFORM_WINDOWS
-#if HN_DYNAMIC_LINK
-	#ifdef HN_BUILD_DLL
-		#define HONEY_API __declspec(dllexport)
+// Platform detection using predefined macros
+#ifdef _WIN32
+	/* Windows x64/x86 */
+	#ifdef _WIN64
+		/* Windows x64  */
+		#define HN_PLATFORM_WINDOWS
 	#else
-		#define HONEY_API __declspec(dllimport)
+		/* Windows x86 */
+		#error "x86 Builds are not supported!"
+	#endif
+#elif defined(__APPLE__) || defined(__MACH__)
+	#include <TargetConditionals.h>
+	/* TARGET_OS_MAC exists on all the platforms
+	 * so we must check all of them (in this order)
+	 * to ensure that we're running on MAC
+	 * and not some other Apple platform */
+	#if TARGET_IPHONE_SIMULATOR == 1
+		#error "IOS simulator is not supported!"
+	#elif TARGET_OS_IPHONE == 1
+		#define HN_PLATFORM_IOS
+		#error "IOS is not supported!"
+	#elif TARGET_OS_MAC == 1
+		#define HN_PLATFORM_MACOS
+		#error "MacOS is not supported!"
+	#else
+		#error "Unknown Apple platform!"
+	#endif
+/* We also have to check __ANDROID__ before __linux__
+ * since android is based on the linux kernel
+ * it has __linux__ defined */
+#elif defined(__ANDROID__)
+	#define HN_PLATFORM_ANDROID
+	#error "Android is not supported!"
+#elif defined(__linux__)
+	#define HN_PLATFORM_LINUX
+	#error "Linux is not supported!"
+#else
+	/* Unknown compiler/platform */
+	#error "Unknown platform!"
+#endif // End of platform detection
+
+// DLL support
+#ifdef HN_PLATFORM_WINDOWS
+	#if HN_DYNAMIC_LINK
+		#ifdef HN_BUILD_DLL
+			#define HONEY_API __declspec(dllexport)
+		#else
+			#define HONEY_API __declspec(dllimport)
+		#endif
+	#else
+		#define HONEY_API
 	#endif
 #else
-	#define HONEY_API
-#endif
-#else
 	#error Honey only supports Windows!
-#endif
+#endif // End of DLL support
 
 #ifdef HN_DEBUG
 	#define HN_ENABLE_ASSERTS
--- premake5.lua
+++ premake5.lua
@@ -83,7 +83,6 @@ project "Honey"
 
 		defines
 		{
-			"HN_PLATFORM_WINDOWS",
 			"HN_BUILD_DLL",
 			"GLFW_INCLUDE_NONE"
 		}
@@ -134,11 +133,6 @@ project "Sandbox"
 
 	filter "system:windows"
 		systemversion "latest"
-
-		defines
-		{
-			"HN_PLATFORM_WINDOWS"
-		}
 
 	filter "configurations:Debug"
 		defines "HN_DEBUG"
