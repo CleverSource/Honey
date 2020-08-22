5c665d2e8a7911adce23067d07db995f32487c1d
CleverSource <stunthound@gmail.com>
2020-08-22 18:40:40 -0400

    Fix Build warnings to do with BufferElement Offset Type

--- Honey/src/Platform/OpenGL/OpenGLVertexArray.cpp
+++ Honey/src/Platform/OpenGL/OpenGLVertexArray.cpp
@@ -63,7 +63,7 @@ namespace Honey {
 				ShaderDataTypeToOpenGLBaseType(element.Type),
 				element.Normalized ? GL_TRUE : GL_FALSE,
 				layout.GetStride(),
-				(const void*)(intptr_t)element.Offset);
+				(const void*)element.Offset);
 			m_VertexBufferIndex++;
 		}
 
