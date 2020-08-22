8259c889f6d5516ca537bd225d906b4c1ecc1b86
CleverSource <stunthound@gmail.com>
2020-08-22 13:16:19 -0400

    Added check for OpenGL version

--- Honey/src/Platform/OpenGL/OpenGLContext.cpp
+++ Honey/src/Platform/OpenGL/OpenGLContext.cpp
@@ -24,6 +24,14 @@ namespace Honey {
 		HN_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
 		HN_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
 
+	#ifdef HN_ENABLE_ASSERTS
+		int versionMajor;
+		int versionMinor;
+		glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+		glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
+
+		HN_CORE_ASSERT(versionMajor > 4 || (versionMajor == 4 && versionMinor >= 5), "Honey requires at least OpenGL version 4.5!");
+	#endif
 	}
 
 	void OpenGLContext::SwapBuffers()
