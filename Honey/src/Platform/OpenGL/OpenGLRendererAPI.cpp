b52572a87a8e421f3244d15394cb2a96c0bd8cfb
CleverSource <stunthound@gmail.com>
2020-08-24 00:10:26 -0400

    Logging OpenGL messages
    Logging all messages with OpenGL severity low and higher

--- Honey/src/Platform/OpenGL/OpenGLRendererAPI.cpp
+++ Honey/src/Platform/OpenGL/OpenGLRendererAPI.cpp
@@ -5,9 +5,37 @@
 
 namespace Honey {
 
+	void OpenGLMessageCallback(
+		unsigned source,
+		unsigned type,
+		unsigned id,
+		unsigned severity,
+		int length,
+		const char* message,
+		const void* userParam)
+	{
+		switch (severity)
+		{
+			case GL_DEBUG_SEVERITY_HIGH:         HN_CORE_CRITICAL(message); return;
+			case GL_DEBUG_SEVERITY_MEDIUM:       HN_CORE_ERROR(message); return;
+			case GL_DEBUG_SEVERITY_LOW:          HN_CORE_WARN(message); return;
+			case GL_DEBUG_SEVERITY_NOTIFICATION: HN_CORE_TRACE(message); return;
+		}
+
+		HN_CORE_ASSERT(false, "Unknown severity level!");
+	}
+
 	void OpenGLRendererAPI::Init()
 	{
 		HN_PROFILE_FUNCTION();
+
+	#ifdef HN_DEBUG
+		glEnable(GL_DEBUG_OUTPUT);
+		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+		glDebugMessageCallback(OpenGLMessageCallback, nullptr);
+
+		glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_NOTIFICATION, 0, NULL, GL_FALSE);
+	#endif
 
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
--- Honey/src/Platform/Windows/WindowsWindow.cpp
+++ Honey/src/Platform/Windows/WindowsWindow.cpp
@@ -5,6 +5,8 @@
 #include "Honey/Events/MouseEvent.h"
 #include "Honey/Events/KeyEvent.h"
 
+#include "Honey/Renderer/Renderer.h"
+
 #include "Platform/OpenGL/OpenGLContext.h"
 
 namespace Honey {
@@ -55,6 +57,10 @@ namespace Honey {
 
 		{
 			HN_PROFILE_SCOPE("glfwCreateWindow");
+		#if defined(HN_DEBUG)
+			if (Renderer::GetAPI() == RendererAPI::API::OpenGL)
+				glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+		#endif
 			m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
 			++s_GLFWWindowCount;
 		}
