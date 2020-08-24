0e8e0aac8d704594491ab810a4263356b99d5d00
CleverSource <stunthound@gmail.com>
2020-08-24 00:04:03 -0400

    Auto deducing an availabe __FUNCSIG__ definition
    Created `HN_FUNC_SIG` macro to deduce a valid pretty function name macro as `__FUNCSIG__` isn't available on all compilers

--- Honey/src/Honey/Debug/Instrumentor.h
+++ Honey/src/Honey/Debug/Instrumentor.h
@@ -124,10 +124,31 @@ namespace Honey {
 
 #define HN_PROFILE 1
 #if HN_PROFILE
+	// Resolve which function signature macro will be used. Note that this only
+	// is resolved when the (pre)compiler starts, so the syntax highlighting
+	// could mark the wrong one in your editor!
+	#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+		#define HN_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__DMC__) && (__DMC__ >= 0x810)
+		#define HN_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__FUNCSIG__)
+		#define HN_FUNC_SIG __FUNCSIG__
+	#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+		#define HN_FUNC_SIG __FUNCTION__
+	#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+		#define HN_FUNC_SIG __FUNC__
+	#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+		#define HN_FUNC_SIG __func__
+	#elif defined(__cplusplus) && (__cplusplus >= 201103)
+		#define HN_FUNC_SIG __func__
+	#else
+		#define HN_FUNC_SIG "HZ_FUNC_SIG unknown!"
+	#endif
+
 	#define HN_PROFILE_BEGIN_SESSION(name, filepath) ::Honey::Instrumentor::Get().BeginSession(name, filepath)
 	#define HN_PROFILE_END_SESSION() ::Honey::Instrumentor::Get().EndSession()
 	#define HN_PROFILE_SCOPE(name) ::Honey::InstrumentationTimer timer##__LINE__(name);
-	#define HN_PROFILE_FUNCTION() HN_PROFILE_SCOPE(__FUNCSIG__)
+	#define HN_PROFILE_FUNCTION() HN_PROFILE_SCOPE(HN_FUNC_SIG)
 #else
 	#define HN_PROFILE_BEGIN_SESSION(name, filepath)
 	#define HN_PROFILE_END_SESSION()
