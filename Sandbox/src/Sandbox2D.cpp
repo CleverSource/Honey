690a9da38cc5444ecf9718cc1812db42160eb63c
CleverSource <stunthound@gmail.com>
2020-08-24 02:23:09 -0400

    Updated Sandbox2D

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -38,9 +38,9 @@ void Sandbox2D::OnUpdate(Honey::Timestep ts)
 	{
 		HN_PROFILE_SCOPE("Renderer Draw");
 		Honey::Renderer2D::BeginScene(m_CameraController.GetCamera());
-		Honey::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+		Honey::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, glm::radians(-45.0f), { 0.8f, 0.2f, 0.3f, 1.0f });
 		Honey::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-		Honey::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture);
+		Honey::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
 		Honey::Renderer2D::EndScene();
 	}
 }
