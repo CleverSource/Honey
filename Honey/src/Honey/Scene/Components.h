8b8fa488a47d14ba34d4e8c63514c60d87759904
CleverSource <stunthound@gmail.com>
2020-09-02 11:37:43 -0400

    Added SceneCamera + camera resizing support

--- Honey/src/Honey/Scene/Components.h
+++ Honey/src/Honey/Scene/Components.h
@@ -2,7 +2,7 @@
 
 #include <glm/glm.hpp>
 
-#include "Honey/Renderer/Camera.h"
+#include "SceneCamera.h"
 
 namespace Honey {
 
@@ -41,13 +41,12 @@ namespace Honey {
 
 	struct CameraComponent
 	{
-		Honey::Camera Camera;
+		SceneCamera Camera;
 		bool Primary = true; // TODO: think about moving to Scene
+		bool FixedAspectRatio = false;
 
 		CameraComponent() = default;
 		CameraComponent(const CameraComponent&) = default;
-		CameraComponent(const glm::mat4& projection)
-			: Camera(projection) {}
 	};
 
 }
