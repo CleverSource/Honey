b0adb144ffc32209bf7a52b2081dc1b3298b5181
CleverSource <stunthound@gmail.com>
2020-10-19 22:14:32 -0400

    Multiple small improvements
    - BufferLayout constructor passing initializer_list by value (they are just pointers)
    - Added const qualifiers to Renderer2D::GetTotalVertexCount()
    - Added const qualifiers to Renderer2D::GetTotalIndexCount()
    - VertexArray now using default virtual destructor

--- Honey/src/Honey/Renderer/Buffer.h
+++ Honey/src/Honey/Renderer/Buffer.h
@@ -70,7 +70,7 @@ namespace Honey {
 	public:
 		BufferLayout() {}
 
-		BufferLayout(const std::initializer_list<BufferElement>& elements) 
+		BufferLayout(std::initializer_list<BufferElement> elements) 
 			: m_Elements(elements)
 		{
 			CalculateOffsetsAndStride();
--- Honey/src/Honey/Renderer/VertexArray.h
+++ Honey/src/Honey/Renderer/VertexArray.h
@@ -8,7 +8,7 @@ namespace Honey {
 	class VertexArray
 	{
 	public:
-		virtual ~VertexArray() {}
+		virtual ~VertexArray() = default;
 
 		virtual void Bind() const = 0;
 		virtual void Unbind() const = 0;
