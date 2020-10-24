26b9557c75c3f6c4bf4f662f313bed1d7ccbd10a
CleverSource <stunthound@gmail.com>
2020-10-24 08:08:12 -0400

    Added New, Open, and Save As file menu options to Honeynut + Win32 open/save file dialogs

--- Honeynut/src/EditorLayer.h
+++ Honeynut/src/EditorLayer.h
@@ -18,6 +18,12 @@ namespace Honey {
 		virtual void OnImGuiRender() override;
 		void OnEvent(Event& e) override;
 	private:
+		bool OnKeyPressed(KeyPressedEvent& e);
+
+		void NewScene();
+		void OpenScene();
+		void SaveSceneAs();
+	private:
 		Honey::OrthographicCameraController m_CameraController;
 
 		// Temp
--- Honeynut/src/Panels/SceneHierarchyPanel.cpp
+++ Honeynut/src/Panels/SceneHierarchyPanel.cpp
@@ -17,6 +17,7 @@ namespace Honey {
 	void SceneHierarchyPanel::SetContext(const Ref<Scene>& context)
 	{
 		m_Context = context;
+		m_SelectionContext = {};
 	}
 
 	void SceneHierarchyPanel::OnImGuiRender()
