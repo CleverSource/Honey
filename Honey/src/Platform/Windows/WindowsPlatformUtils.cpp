fb788e99c6c1c8fca02955b462fa25f5e2887fa1
CleverSource <stunthound@gmail.com>
2020-12-03 12:13:44 -0500

    Added default extension string when saving a scene
    If the extension is not specified, the file was not saved with a .honey extension

--- Honey/src/Platform/Windows/WindowsPlatformUtils.cpp
+++ Honey/src/Platform/Windows/WindowsPlatformUtils.cpp
@@ -1,6 +1,7 @@
 #include "hnpch.h"
 #include "Honey/Utils/PlatformUtils.h"
 
+#include <sstream>
 #include <commdlg.h>
 #include <GLFW/glfw3.h>
 #define GLFW_EXPOSE_NATIVE_WIN32
@@ -40,6 +41,10 @@ namespace Honey {
 		ofn.nMaxFile = sizeof(szFile);
 		ofn.lpstrFilter = filter;
 		ofn.nFilterIndex = 1;
+
+		// Sets the default extension by extracting it from the filter
+		ofn.lpstrDefExt = strchr(filter, '\0') + 1;
+
 		ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
 		if (GetSaveFileNameA(&ofn) == TRUE)
 		{
