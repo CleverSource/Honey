3a97d104dc6bec867352bd60c60b0830abe06fc1
CleverSource <stunthound@gmail.com>
2021-02-07 16:49:38 -0500

    Added Framebuffer API to clear texture attachments

--- Honey/src/Platform/OpenGL/OpenGLFramebuffer.cpp
+++ Honey/src/Platform/OpenGL/OpenGLFramebuffer.cpp
@@ -76,6 +76,18 @@ namespace Honey {
 			return false;
 		}
 
+		static GLenum HoneyFBTextureFormatToGL(FramebufferTextureFormat format)
+		{
+			switch (format)
+			{
+				case FramebufferTextureFormat::RGBA8:       return GL_RGBA8;
+				case FramebufferTextureFormat::RED_INTEGER: return GL_RED_INTEGER;
+			}
+
+			HN_CORE_ASSERT(false);
+			return 0;
+		}
+
 	}
 
 	OpenGLFramebuffer::OpenGLFramebuffer(const FramebufferSpecification& spec)
@@ -198,6 +210,15 @@ namespace Honey {
 		int pixelData;
 		glReadPixels(x, y, 1, 1, GL_RED_INTEGER, GL_INT, &pixelData);
 		return pixelData;
+	}
+
+	void OpenGLFramebuffer::ClearAttachment(uint32_t attachmentIndex, int value)
+	{
+		HN_CORE_ASSERT(attachmentIndex < m_ColorAttachments.size());
+
+		auto& spec = m_ColorAttachmentSpecifications[attachmentIndex];
+		glClearTexImage(m_ColorAttachments[attachmentIndex], 0, 
+			Utils::HoneyFBTextureFormatToGL(spec.TextureFormat), GL_INT, &value);
 	}
 
 }
