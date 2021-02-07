fc099a5c209f9a3a339332003c1c55bb679b234e
CleverSource <stunthound@gmail.com>
2021-02-07 16:22:29 -0500

    Added missing 'SetFloat2' in Renderer for completeness

--- Honey/src/Platform/OpenGL/OpenGLShader.cpp
+++ Honey/src/Platform/OpenGL/OpenGLShader.cpp
@@ -218,6 +218,13 @@ namespace Honey {
 		UploadUniformFloat(name, value);
 	}
 
+	void OpenGLShader::SetFloat2(const std::string& name, const glm::vec2& value)
+	{
+		HN_PROFILE_FUNCTION();
+
+		UploadUniformFloat2(name, value);
+	}
+
 	void OpenGLShader::SetFloat3(const std::string& name, const glm::vec3& value)
 	{
 		HN_PROFILE_FUNCTION();
