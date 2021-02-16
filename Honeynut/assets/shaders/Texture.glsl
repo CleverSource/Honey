424f785a66cdc0c1733bad521d47781aca584046
CleverSource <stunthound@gmail.com>
2021-02-16 15:28:05 -0500

    Fixed wrong viewport bounds when viewport tab bar is visible

--- Honeynut/assets/shaders/Texture.glsl
+++ Honeynut/assets/shaders/Texture.glsl
@@ -48,16 +48,16 @@ void main()
 
 	switch(int(v_TexIndex))
 	{
-		case  0: texColor *= texture(u_Textures[0], v_TexCoord * v_TilingFactor); break;
-		case  1: texColor *= texture(u_Textures[1], v_TexCoord * v_TilingFactor); break;
-		case  2: texColor *= texture(u_Textures[2], v_TexCoord * v_TilingFactor); break;
-		case  3: texColor *= texture(u_Textures[3], v_TexCoord * v_TilingFactor); break;
-		case  4: texColor *= texture(u_Textures[4], v_TexCoord * v_TilingFactor); break;
-		case  5: texColor *= texture(u_Textures[5], v_TexCoord * v_TilingFactor); break;
-		case  6: texColor *= texture(u_Textures[6], v_TexCoord * v_TilingFactor); break;
-		case  7: texColor *= texture(u_Textures[7], v_TexCoord * v_TilingFactor); break;
-		case  8: texColor *= texture(u_Textures[8], v_TexCoord * v_TilingFactor); break;
-		case  9: texColor *= texture(u_Textures[9], v_TexCoord * v_TilingFactor); break;
+		case  0: texColor *= texture(u_Textures[ 0], v_TexCoord * v_TilingFactor); break;
+		case  1: texColor *= texture(u_Textures[ 1], v_TexCoord * v_TilingFactor); break;
+		case  2: texColor *= texture(u_Textures[ 2], v_TexCoord * v_TilingFactor); break;
+		case  3: texColor *= texture(u_Textures[ 3], v_TexCoord * v_TilingFactor); break;
+		case  4: texColor *= texture(u_Textures[ 4], v_TexCoord * v_TilingFactor); break;
+		case  5: texColor *= texture(u_Textures[ 5], v_TexCoord * v_TilingFactor); break;
+		case  6: texColor *= texture(u_Textures[ 6], v_TexCoord * v_TilingFactor); break;
+		case  7: texColor *= texture(u_Textures[ 7], v_TexCoord * v_TilingFactor); break;
+		case  8: texColor *= texture(u_Textures[ 8], v_TexCoord * v_TilingFactor); break;
+		case  9: texColor *= texture(u_Textures[ 9], v_TexCoord * v_TilingFactor); break;
 		case 10: texColor *= texture(u_Textures[10], v_TexCoord * v_TilingFactor); break;
 		case 11: texColor *= texture(u_Textures[11], v_TexCoord * v_TilingFactor); break;
 		case 12: texColor *= texture(u_Textures[12], v_TexCoord * v_TilingFactor); break;
