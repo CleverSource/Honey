624babbbf417fd85740fc32995b52b3afee21ec5
Ryan Caldwell <stunthound@gmail.com>
2021-07-07 11:13:36 -0400

    Content Browser panel thumbnail layout

--- Honeynut/src/Panels/ContentBrowserPanel.h
+++ Honeynut/src/Panels/ContentBrowserPanel.h
@@ -2,6 +2,8 @@
 
 #include <filesystem>
 
+#include "Honey/Renderer/Texture.h"
+
 namespace Honey {
 
 	class ContentBrowserPanel 
@@ -12,6 +14,8 @@ namespace Honey {
 		void OnImGuiRender();
 	private:
 		std::filesystem::path m_CurrentDirectory;
+		Ref<Texture2D> m_DirectoryIcon;
+		Ref<Texture2D> m_FileIcon;
 	};
 
 }
