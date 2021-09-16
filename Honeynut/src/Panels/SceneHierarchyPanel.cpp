44101026f6081434442fb98c2a3742124817a0d5
Ryan Caldwell <stunthound@gmail.com>
2021-09-15 23:52:31 -0400

    Stability + bug fixes
    - Added IsLoaded to Texture to see if textures were actually loaded
    - We now catch YAML::ParserException (although this isn't a reliable way of catching invalid YAML files)
    - Added .honey extension checking to EditorLayer::OpenScene

--- Honeynut/src/Panels/SceneHierarchyPanel.cpp
+++ Honeynut/src/Panels/SceneHierarchyPanel.cpp
@@ -339,7 +339,11 @@ namespace Honey {
 				{
 					const wchar_t* path = (const wchar_t*)payload->Data;
 					std::filesystem::path texturePath = std::filesystem::path(g_AssetPath) / path;
-					component.Texture = Texture2D::Create(texturePath.string());
+					Ref<Texture2D> texture = Texture2D::Create(texturePath.string());
+					if (texture->IsLoaded())
+						component.Texture = texture;
+					else
+						HN_WARN("Could not load texture {0}", texturePath.filename().string());
 				}
 				ImGui::EndDragDropTarget();
 			}
