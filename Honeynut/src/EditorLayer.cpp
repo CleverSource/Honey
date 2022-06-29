7e25a0bad38de9f7f6331e612bb13f979f1e9b18
Ryan Caldwell <stunthound@gmail.com>
2022-06-29 15:38:38 -0400

    Update EditorLayer.cpp

--- Honeynut/src/EditorLayer.cpp
+++ Honeynut/src/EditorLayer.cpp
@@ -409,7 +409,10 @@ namespace Honey {
 	void EditorLayer::OnEvent(Event& e)
 	{
 		m_CameraController.OnEvent(e);
-		m_EditorCamera.OnEvent(e);
+		if (m_SceneState == SceneState::Edit)
+		{
+			m_EditorCamera.OnEvent(e);
+		}
 
 		EventDispatcher dispatcher(e);
 		dispatcher.Dispatch<KeyPressedEvent>(HN_BIND_EVENT_FN(EditorLayer::OnKeyPressed));
