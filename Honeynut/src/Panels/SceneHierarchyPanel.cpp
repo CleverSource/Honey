ae0a86504244ee8b89ae3960fb0893d9b8957a91
Ryan Caldwell <stunthound@gmail.com>
2022-08-30 18:19:56 -0400

    Retrieving public C# Entity class fields
    - Also getting/setting values during runtime

--- Honeynut/src/Panels/SceneHierarchyPanel.cpp
+++ Honeynut/src/Panels/SceneHierarchyPanel.cpp
@@ -322,7 +322,7 @@ namespace Honey {
 			}
 		});
 
-		DrawComponent<ScriptComponent>("Script", entity, [](auto& component)
+		DrawComponent<ScriptComponent>("Script", entity, [entity](auto& component) mutable
 		{
 			bool scriptClassExists = ScriptEngine::EntityClassExists(component.ClassName);
 
@@ -334,6 +334,25 @@ namespace Honey {
 
 			if (ImGui::InputText("Class", buffer, sizeof(buffer)))
 				component.ClassName = buffer;
+
+			// Fields
+			Ref<ScriptInstance> scriptInstance = ScriptEngine::GetEntityScriptInstance(entity.GetUUID());
+			if (scriptInstance)
+			{
+				const auto& fields = scriptInstance->GetScriptClass()->GetFields();
+
+				for (const auto& [name, field] : fields)
+				{
+					if (field.Type == ScriptFieldType::Float)
+					{
+						float data = scriptInstance->GetFieldValue<float>(name);
+						if (ImGui::DragFloat(name.c_str(), &data))
+						{
+							scriptInstance->SetFieldValue(name, data);
+						}
+					}
+				}
+			}
 
 			if (!scriptClassExists)
 				ImGui::PopStyleColor();
